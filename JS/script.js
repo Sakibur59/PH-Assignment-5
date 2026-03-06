a715cc2bf60e14f969dbf7d4966a4d63a1eff3a1
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-07 03:10:57 +0600

    open and close list border color adjusted

--- JS/script.js
+++ JS/script.js
@@ -84,20 +84,118 @@ function toggle(id) {
      openIssueList();
   } else if(id === 'all-btn'){
      displayALLIssues(all);
-  }
-  if(id === 'close-btn'){
+  } else if(id === 'close-btn'){
      closeIssueList();
   }
 }
 
+
+const displayOpenIssuesList = (issues) => {
+  const issuesContainer = document.getElementById("issues-container");
+    issuesContainer.innerHTML = "";
+  issues.forEach((issue) => {
+    const lvl =
+      issue.labels
+        .map((label) => {
+          if (label === "enhancement") {
+            return `<span class="bg-[#D97706] text-[black] border rounded-md px-3 py-1 ">${label.toUpperCase()}</span>`;
+          }
+          if (label === "bug") {
+            return `<span class="bg-[#D97706] text-[black] border rounded-md px-3 py-1 ">${label.toUpperCase()}</span>`;
+          }
+          if (label === "help wanted") {
+            return `<span class="bg-[#D97706] text-[black] px-3 py-1 border rounded-md ">${label.toUpperCase()}</span>`;
+          }
+          if (label === "good first issue") {
+            return `<span class="bg-[#D97706] text-[black] border rounded-md px-3 py-1 ">${label.toUpperCase()}</span>`;
+          }
+          if (label === "documentation") {
+            return `<span class="bg-[#D97706] text-[black] border rounded-md px-3 py-1 ">${label.toUpperCase()}</span>`;
+          }
+        })
+        .join("") || "";
+
+    let border = "border-[#00A96E]";
+    let logo = "./assets/Open-Status.png";
+
+    
+
+    const issueDiv = document.createElement("div");
+    issueDiv.innerHTML = `
+        <div class="p-4 space-y-3 shadow-sm border-t-4 ${border} rounded-md">
+          <div class="flex justify-between">
+            <img src=${logo} alt="" />
+            <p class="text-[#EF4444] ">${issue.priority}</p>
+          </div>
+          <h3 class="text-black font-semibold">${issue.title}</h3>
+          <p class="text-[#64748B] line-clamp-2">${issue.description}</p>
+          <div class="flex flex-wrap gap-2 text-[12px]">${lvl}</div>
+          <p class="text-[#64748B]">#1by john_doe</p>
+          <p class="text-[#64748B]">1/15/2024</p>
+        </div>
+        `;
+    issuesContainer.append(issueDiv);
+    totalIssues.innerText = issues.length;
+  });
+};
+
 const openIssueList = () =>{
     const openIssue = all.filter(item => item.status === 'open');
-    displayALLIssues(openIssue);
+    displayOpenIssuesList(openIssue);
 }
+
+const displayCloseIssuesList = (issues) => {
+  const issuesContainer = document.getElementById("issues-container");
+    issuesContainer.innerHTML = "";
+  issues.forEach((issue) => {
+    const lvl =
+      issue.labels
+        .map((label) => {
+          if (label === "enhancement") {
+            return `<span class="bg-[#D97706] text-[black] border rounded-md px-3 py-1 ">${label.toUpperCase()}</span>`;
+          }
+          if (label === "bug") {
+            return `<span class="bg-[#D97706] text-[black] border rounded-md px-3 py-1 ">${label.toUpperCase()}</span>`;
+          }
+          if (label === "help wanted") {
+            return `<span class="bg-[#D97706] text-[black] px-3 py-1 border rounded-md ">${label.toUpperCase()}</span>`;
+          }
+          if (label === "good first issue") {
+            return `<span class="bg-[#D97706] text-[black] border rounded-md px-3 py-1 ">${label.toUpperCase()}</span>`;
+          }
+          if (label === "documentation") {
+            return `<span class="bg-[#D97706] text-[black] border rounded-md px-3 py-1 ">${label.toUpperCase()}</span>`;
+          }
+        })
+        .join("") || "";
+
+    let border = "border-[#A855F7]";
+    let logo = "./assets/Closed-Status.png";
+
+   
+
+    const issueDiv = document.createElement("div");
+    issueDiv.innerHTML = `
+        <div class="p-4 space-y-3 shadow-sm border-t-4 ${border} rounded-md">
+          <div class="flex justify-between">
+            <img src=${logo} alt="" />
+            <p class="text-[#EF4444] ">${issue.priority}</p>
+          </div>
+          <h3 class="text-black font-semibold">${issue.title}</h3>
+          <p class="text-[#64748B] line-clamp-2">${issue.description}</p>
+          <div class="flex flex-wrap gap-2 text-[12px]">${lvl}</div>
+          <p class="text-[#64748B]">#1by john_doe</p>
+          <p class="text-[#64748B]">1/15/2024</p>
+        </div>
+        `;
+    issuesContainer.append(issueDiv);
+    totalIssues.innerText = issues.length;
+  });
+};
 
 const closeIssueList = () =>{
     const closeIssue = all.filter(item => item.status === 'closed');
-    displayALLIssues(closeIssue);
+    displayCloseIssuesList(closeIssue);
 }
 
 loadData();
