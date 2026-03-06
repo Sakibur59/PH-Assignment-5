ae1f4e2f78076a95b0568fa40ecd1049bc3ba096
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-07 03:55:12 +0600

    Implemented functionality of search section

--- JS/script.js
+++ JS/script.js
@@ -3,15 +3,15 @@ let loadData = () => {
   fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then((res) => res.json())
     .then((json) => {
-        all=json.data;
-        displayALLIssues(all);
+      all = json.data;
+      displayALLIssues(all);
     });
 };
 const totalIssues = document.getElementById("total-issues");
 
 const displayALLIssues = (issues) => {
   const issuesContainer = document.getElementById("issues-container");
-    issuesContainer.innerHTML = "";
+  issuesContainer.innerHTML = "";
   issues.forEach((issue) => {
     const lvl =
       issue.labels
@@ -52,8 +52,8 @@ const displayALLIssues = (issues) => {
           <h3 class="text-black font-semibold">${issue.title}</h3>
           <p class="text-[#64748B] line-clamp-2">${issue.description}</p>
           <div class="flex flex-wrap gap-2 text-[12px]">${lvl}</div>
-          <p class="text-[#64748B]">#1by john_doe</p>
-          <p class="text-[#64748B]">1/15/2024</p>
+          <p class="text-[#64748B]">#${issue.id} by ${issue.author}</p>
+          <p class="text-[#64748B]">Assignee: ${issue.assignee ? issue.assignee : "Unassigned"}</p>
         </div>
         `;
     issuesContainer.append(issueDiv);
@@ -61,16 +61,14 @@ const displayALLIssues = (issues) => {
   });
 };
 
-
-
 let allBtn = document.getElementById("all-btn");
 let openBtn = document.getElementById("open-btn");
 let closeBtn = document.getElementById("close-btn");
 
 function toggle(id) {
-  allBtn.classList.remove( "bg-[#4A00FF]" ,"text-white");
-  openBtn.classList.remove( "bg-[#4A00FF]" ,"text-white");
-  closeBtn.classList.remove( "bg-[#4A00FF]" ,"text-white");
+  allBtn.classList.remove("bg-[#4A00FF]", "text-white");
+  openBtn.classList.remove("bg-[#4A00FF]", "text-white");
+  closeBtn.classList.remove("bg-[#4A00FF]", "text-white");
 
   allBtn.classList.add("bg-white", "text-[#64748B]");
   openBtn.classList.add("bg-white", "text-[#64748B]");
@@ -80,19 +78,18 @@ function toggle(id) {
   selected.classList.remove("bg-white", "text-[#64748B]", "border-gray-300");
   selected.classList.add("bg-[#4A00FF]", "text-white");
 
-  if(id === 'open-btn'){
-     openIssueList();
-  } else if(id === 'all-btn'){
-     displayALLIssues(all);
-  } else if(id === 'close-btn'){
-     closeIssueList();
+  if (id === "open-btn") {
+    openIssueList();
+  } else if (id === "all-btn") {
+    displayALLIssues(all);
+  } else if (id === "close-btn") {
+    closeIssueList();
   }
 }
 
-
 const displayOpenIssuesList = (issues) => {
   const issuesContainer = document.getElementById("issues-container");
-    issuesContainer.innerHTML = "";
+  issuesContainer.innerHTML = "";
   issues.forEach((issue) => {
     const lvl =
       issue.labels
@@ -118,8 +115,6 @@ const displayOpenIssuesList = (issues) => {
     let border = "border-[#00A96E]";
     let logo = "./assets/Open-Status.png";
 
-    
-
     const issueDiv = document.createElement("div");
     issueDiv.innerHTML = `
         <div class="p-4 space-y-3 shadow-sm border-t-4 ${border} rounded-md">
@@ -130,8 +125,8 @@ const displayOpenIssuesList = (issues) => {
           <h3 class="text-black font-semibold">${issue.title}</h3>
           <p class="text-[#64748B] line-clamp-2">${issue.description}</p>
           <div class="flex flex-wrap gap-2 text-[12px]">${lvl}</div>
-          <p class="text-[#64748B]">#1by john_doe</p>
-          <p class="text-[#64748B]">1/15/2024</p>
+          <p class="text-[#64748B]">#${issue.id} by ${issue.author}</p>
+          <p class="text-[#64748B]">Assignee: ${issue.assignee ? issue.assignee : "Unassigned"}</p>
         </div>
         `;
     issuesContainer.append(issueDiv);
@@ -139,14 +134,14 @@ const displayOpenIssuesList = (issues) => {
   });
 };
 
-const openIssueList = () =>{
-    const openIssue = all.filter(item => item.status === 'open');
-    displayOpenIssuesList(openIssue);
-}
+const openIssueList = () => {
+  const openIssue = all.filter((item) => item.status === "open");
+  displayOpenIssuesList(openIssue);
+};
 
 const displayCloseIssuesList = (issues) => {
   const issuesContainer = document.getElementById("issues-container");
-    issuesContainer.innerHTML = "";
+  issuesContainer.innerHTML = "";
   issues.forEach((issue) => {
     const lvl =
       issue.labels
@@ -172,8 +167,6 @@ const displayCloseIssuesList = (issues) => {
     let border = "border-[#A855F7]";
     let logo = "./assets/Closed-Status.png";
 
-   
-
     const issueDiv = document.createElement("div");
     issueDiv.innerHTML = `
         <div class="p-4 space-y-3 shadow-sm border-t-4 ${border} rounded-md">
@@ -184,8 +177,8 @@ const displayCloseIssuesList = (issues) => {
           <h3 class="text-black font-semibold">${issue.title}</h3>
           <p class="text-[#64748B] line-clamp-2">${issue.description}</p>
           <div class="flex flex-wrap gap-2 text-[12px]">${lvl}</div>
-          <p class="text-[#64748B]">#1by john_doe</p>
-          <p class="text-[#64748B]">1/15/2024</p>
+          <p class="text-[#64748B]">#${issue.id} by ${issue.author}</p>
+          <p class="text-[#64748B]">Assignee: ${issue.assignee ? issue.assignee : "Unassigned"}</p>
         </div>
         `;
     issuesContainer.append(issueDiv);
@@ -193,9 +186,18 @@ const displayCloseIssuesList = (issues) => {
   });
 };
 
-const closeIssueList = () =>{
-    const closeIssue = all.filter(item => item.status === 'closed');
-    displayCloseIssuesList(closeIssue);
-}
+const closeIssueList = () => {
+  const closeIssue = all.filter((item) => item.status === "closed");
+  displayCloseIssuesList(closeIssue);
+};
 
 loadData();
+
+document.getElementById("search-btn").addEventListener("click", () => {
+  const searchInput = document.getElementById("search-input");
+  const searchValue = searchInput.value.trim().toLowerCase();
+  console.log(searchValue);
+
+    const filter = all.filter((issue)=>issue.title.toLowerCase().includes(searchValue));
+    displayALLIssues(filter);
+});
