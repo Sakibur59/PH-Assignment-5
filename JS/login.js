55566fb648cb4c6e3604d8c812b0bc85c5f945b6
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-06 21:51:10 +0600

    login page full functionality added

---
+++ JS/login.js
@@ -0,0 +1,15 @@
+document.getElementById("login-btn").addEventListener("click",()=>{
+    const user = document.getElementById("input-login");
+    const username = user.value;
+
+    const pass = document.getElementById("password-login");
+    const password = pass.value;
+
+    if(username === 'admin' && password ==='admin123'){
+        alert("Login Success");
+        window.location.assign("/main.html");
+    } else{
+        alert("Login Failed")
+        return;
+    }
+})
